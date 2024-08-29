8a025aab6b94a402b329cf37ce39afa487599ac5
mr-taqi <syedtaqiuddin313@gmail.com>
2024-08-30 00:55:31 +0530

    day 3

---
+++ Frontend/User/src/Components/Footer.jsx
@@ -0,0 +1,12 @@
+export function Footer() {
+  return (
+
+    <>
+    <section>
+      <div className="px-4 text-center py-8">
+        <p className="text-sm text-gray-600">2020 &copy; All rights reserved</p>
+      </div>
+    </section>
+    </>
+  )
+}
