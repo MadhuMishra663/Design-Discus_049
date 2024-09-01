1c1c1aae6556248d14408f51bbc86c2a3d461fa7
mr-taqi <syedtaqiuddin313@gmail.com>
2024-09-01 15:55:26 +0530

    day 5 code done

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,10 +1,32 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { BrowserRouter } from 'react-router-dom'
+import { Provider } from 'react-redux'
+import { configureStore } from '@reduxjs/toolkit'
+import rootReducer from './reducer/index';
+import { Toaster } from 'react-hot-toast'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+
+    
+
+const store = configureStore({
+  reducer: rootReducer
+});
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <BrowserRouter>
+    <Provider store={store}>
+      <React.StrictMode>
+        <App />
+        <Toaster />
+      </React.StrictMode>
+    </Provider>
+  </BrowserRouter>
 )
+
+
+
+
+
